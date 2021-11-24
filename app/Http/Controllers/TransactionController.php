d15408abecbc8dbda25bdc7bdff1ec13ace9700f
haqqi <haqqitkj1@gmail.com>
2021-11-24 11:55:18 +0700

    delete duplicate use import

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,12 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-use App\Models\Transaction;
-use Illuminate\Database\QueryException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-
 class TransactionController extends Controller
 {
     /**
